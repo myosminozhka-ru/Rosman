55c61305aa34161a9ffc24f74f447be0b5b6393e
Alexei Savusya <alexey.savusya@yellowimages.com>
2023-05-31 13:19:07 +0600

    task № 57403 UI KIT №5 коллапсы/fix анимации

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,29 @@
 window.addEventListener("DOMContentLoaded", () => {
   console.log("Loaded Scripts");
 });
+const buttons = document.getElementsByClassName("collapse-button");
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    const content = this.nextElementSibling;
+    if (content.classList.contains("active")) {
+      content.classList.remove("active");
+    } else {
+      // Закрытие других активных контентов
+      const activeContent = document.querySelector(".collapse-content.active");
+
+      if (activeContent) {
+        activeContent.classList.remove("active");
+      }
+      content.classList.add("active");
+    }
+    if ( this.classList.contains("active")) {
+      this.classList.remove("active");
+    } else {
+      const activeBtn = document.querySelector(".collapse-button.active");
+      if (activeBtn) {
+        activeBtn.classList.remove("active");
+      }
+      this.classList.add("active")
+    }
+  });
+}
