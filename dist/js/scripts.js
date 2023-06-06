cd6e64e449796fdce5cbc8f478e592aabad47c81
AlexSavusia <mr.savusya@gmail.com>
2023-06-06 15:53:07 +0600

    Ui-kit №6 fix cursor pointer width 100% button and checkbox

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -2,7 +2,39 @@
 window.addEventListener("DOMContentLoaded", () => {
   console.log("Loaded Scripts");
 });
-// collapse открытие и закрытие
+// дропдаун добавление класса active-js с отслеживанием клика вне блока
+const moreButtons = document.getElementsByClassName("more-down-button");
+for (let i = 0; i < moreButtons.length; i++) {
+  moreButtons[i].addEventListener("click", function() {
+    const content = this.nextElementSibling;
+    if (this.classList.contains("active-js")) {
+      this.classList.remove("active-js")
+    } else {
+      this.classList.add("active-js");
+    }
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+      content.classList.add("active-js");
+    }
+  });
+}
+
+document.addEventListener("click", function(event) {
+  const block = document.querySelector(".more-down-button.out.active-js");
+  const targetElement = event.target;
+  if (block != null) {
+    if (!block.contains(targetElement)) {
+    for (let i = 0; i < moreButtons.length; i++) {
+      if ( moreButtons[i].classList.contains("active-js")) {
+        moreButtons[i].nextElementSibling.style.display = "none";
+        moreButtons[i].classList.remove("active-js")
+       }
+      }
+    }
+  }
+});
 const buttons = document.getElementsByClassName("collapse-button");
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function() {
