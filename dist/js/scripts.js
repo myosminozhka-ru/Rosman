1613cdb87cc8a7d162a04448777b856407b19bc1
clamator <kalmah23@gmail.com>
2023-06-06 18:21:05 +0600

    merge main

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,4 @@
 //libraries like jquery etc
-
 console.log("privet");
 const popup = function Popup(el, triggerButton) {
   this.el = el;
@@ -126,6 +125,100 @@ for (let i = 0; i < dropdownTogglers.length; i++) {
       content.style.display = "block";
       content.classList.add("active-js");
     }
+  });
+}
+
+// функция для маски телефона
+let inp = document.querySelector('#phone');
+
+inp.onclick = function() {
+  inp.value = "+";
+}
+
+var old = 0;
+
+inp.onkeydown = function() {
+  var curLen = inp.value.length;
+
+  if (curLen < old){
+    old--;
+    return;
+  }
+
+  if (curLen == 2)
+    inp.value = inp.value + "-";
+
+  if (curLen == 6)
+    inp.value = inp.value + "-";
+
+  if (curLen == 10)
+    inp.value = inp.value + "-";
+
+  if (curLen == 13)
+    inp.value = inp.value + "-";
+
+  if (curLen > 15)
+    inp.value = inp.value.substring(0, inp.value.length - 1);
+
+  old++;
+}
+// карточкам будет дан специальный класс - js_traceable_item,
+// а элементам js-bg-changeable, которые будут менять фон,- js-hovered,
+// событие будет генерироваться на заход в целом на карточку - mouseenter/mouseleave
+
+// по поводу изменения картинки при нажатии на сердечко
+// своя функция тут будет
+
+const addToFavBtns = document.getElementsByClassName(
+  "item_card_add_to_favourite"
+);
+for (let i = 0; i < addToFavBtns.length; i++) {
+  addToFavBtns[i].addEventListener("click", function () {
+    const button = this;
+    button.classList.toggle("js_pressed");
+  });
+}
+
+// функция для изменения фона рандомно для блока новостей и наград,
+
+const traceableItems = document.getElementsByClassName("js_traceable_item");
+console.log(traceableItems);
+for (let i = 0; i < traceableItems.length; i++) {
+  traceableItems[i].addEventListener("mouseenter", function (event) {
+    const colors = [
+      "#06083D",
+      "#311166",
+      "#1F1871",
+      "#1F1871",
+      "#0070EA",
+      "#4498FC",
+      "#4B65CD",
+      "#808080",
+      "#AF74E9",
+      "#33C15B",
+      "#FFF145",
+      "#D9D9D9",
+      "#F5F5F5",
+      // "#ffffff",
+      "#E3E3E2",
+      "#FF0053",
+      "#FF8500",
+      "#F46300",
+    ];
+    function getRandomColor(colorArray) {
+      if (colorArray.length === 0) {
+        return "#ffffff";
+      }
+      const randomIndex = Math.floor(Math.random() * colorArray.length);
+      return colorArray[randomIndex];
+    }
+    this.style.backgroundColor = getRandomColor(colors);
+  });
+}
+
+for (let i = 0; i < traceableItems.length; i++) {
+  traceableItems[i].addEventListener("mouseleave", function (event) {
+    this.style.backgroundColor = "white";
   });
 }
 
