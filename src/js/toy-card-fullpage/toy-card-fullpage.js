d2c5e564a1c4341375c37bb3b62f5469557e0ea4
clamator <kalmah23@gmail.com>
2023-06-16 18:10:00 +0600

    made a breadcrumd mini popup, shorting the text in breadcrumbs while screen is small and some minor updates

--- src/js/toy-card-fullpage/toy-card-fullpage.js
+++ src/js/toy-card-fullpage/toy-card-fullpage.js
@@ -2,15 +2,15 @@ let largeImg = document.querySelector('.largeImg');
 let thumbs = document.querySelectorAll('.thumbs img');
 
 thumbs.forEach(function (thumb) {
-    thumb.addEventListener('click', function (event) {
-        const currentChosen = document.querySelector(".thumbs .js-thumb-chosen");
-        event.preventDefault();
-        largeImg.src = thumb.getAttribute('src');
-        if (currentChosen) {
-            currentChosen.classList.remove("js-thumb-chosen");
-        }
-        thumb.classList.add("js-thumb-chosen")
-    });
+  thumb.addEventListener('click', function (event) {
+    const currentChosen = document.querySelector(".thumbs .js-thumb-chosen");
+    event.preventDefault();
+    largeImg.src = thumb.getAttribute('src');
+    if (currentChosen) {
+      currentChosen.classList.remove("js-thumb-chosen");
+    }
+    thumb.classList.add("js-thumb-chosen")
+  });
 });
 
 // нужна функция, которая будет отслеживать нажатие на js-mdb,
@@ -22,46 +22,48 @@ let cardTextToHide = document.querySelector(".card-fp-hide")
 let cartToTrans = document.querySelector(".card-fp-trans")
 
 if (jsMdb) {
-    jsMdb.onclick = function (event) {
-        cardTextToHide.classList.toggle("js-card-fp-hidden-item")
-        cartToTrans.classList.toggle("card-fp-transparent-text")
+  jsMdb.onclick = function (event) {
+    cardTextToHide.classList.toggle("js-card-fp-hidden-item")
+    if (cartToTrans) {
+      cartToTrans.classList.toggle("card-fp-transparent-text")
     }
+  }
 }
 
 // для таба надо добавлять класс active-js, чтобы он подчеркивался
 const navButtons = document.querySelector(".navigation");
 
 if (navButtons) {
-    const descriptionComponent = document.querySelector(".card-fp-description");
-    const characteristicsComponent = document.querySelector(".card-fp-characteristics");
-    const instructionComponent = document.querySelector(".card-fp-instruction");
+  const descriptionComponent = document.querySelector(".card-fp-description");
+  const characteristicsComponent = document.querySelector(".card-fp-characteristics");
+  const instructionComponent = document.querySelector(".card-fp-instruction");
 
-    navButtons.addEventListener("click", (event) => {
-        event.preventDefault()
-        const currentChosen = navButtons.querySelector(".active-js");
-        const target = event.target.closest(".card-fullpage-tab");
+  navButtons.addEventListener("click", (event) => {
+    event.preventDefault()
+    const currentChosen = navButtons.querySelector(".active-js");
+    const target = event.target.closest(".card-fullpage-tab");
 
-        if (!target || target === currentChosen) return;
+    if (!target || target === currentChosen) return;
 
-        if (currentChosen) {
-            currentChosen.classList.remove("active-js");
-        }
-        target.classList.add("active-js");
+    if (currentChosen) {
+      currentChosen.classList.remove("active-js");
+    }
+    target.classList.add("active-js");
 
-        // Скрыть все компоненты
-        descriptionComponent.classList.add("hidden");
-        characteristicsComponent.classList.add("hidden");
-        instructionComponent.classList.add("hidden");
+    // Скрыть все компоненты
+    descriptionComponent.classList.add("hidden");
+    characteristicsComponent.classList.add("hidden");
+    instructionComponent.classList.add("hidden");
 
-        // Показать выбранный компонент
-        if (target.classList.contains("card-fp-tab-desc")) {
-            descriptionComponent.classList.remove("hidden");
-        } else if (target.classList.contains("card-fp-tab-char")) {
-            characteristicsComponent.classList.remove("hidden");
-        } else if (target.classList.contains("card-fp-tab-instr")) {
-            instructionComponent.classList.remove("hidden");
-        }
-    });
+    // Показать выбранный компонент
+    if (target.classList.contains("card-fp-tab-desc")) {
+      descriptionComponent.classList.remove("hidden");
+    } else if (target.classList.contains("card-fp-tab-char")) {
+      characteristicsComponent.classList.remove("hidden");
+    } else if (target.classList.contains("card-fp-tab-instr")) {
+      instructionComponent.classList.remove("hidden");
+    }
+  });
 }
 
 
@@ -71,29 +73,30 @@ if (navButtons) {
 let jsMdbDescButton = document.querySelector(".js-mdb-desc-button");
 let jsDescriptionWrapper = document.querySelector(".js-card-fullpage-desc-description");
 let jsItemsToHideDesc = document.querySelectorAll(".js-item-to-hide-desc");
-console.log(jsItemsToHideDesc);
 
 if (jsMdbDescButton) {
-    jsMdbDescButton.onclick = function (event) {
-        jsItemsToHideDesc.forEach((item) => {
-            item.classList.toggle("js-card-fp-hidden-item");
-        });
 
-        const isExpanded = jsMdbDescButton.innerText.includes("Свернуть описание");
+  jsMdbDescButton.onclick = function (event) {
+    jsItemsToHideDesc.forEach((item) => {
+      item.classList.toggle("js-card-fp-hidden-item");
+    });
+    console.log("nen")
+    const isExpanded = jsMdbDescButton.innerText.includes("Свернуть описание");
 
-        if (isExpanded) {
-            jsDescriptionWrapper.classList.remove("gapped");
-            jsMdbDescButton.innerHTML = "Развернуть описание<span></span>";
-        } else {
-            jsDescriptionWrapper.classList.add("gapped");
-            jsMdbDescButton.innerHTML = "Свернуть описание<span></span>";
-        }
+    if (isExpanded) {
+      jsDescriptionWrapper.classList.remove("gapped");
+      jsMdbDescButton.innerHTML = "Развернуть описание<span></span>";
+    } else {
+      jsDescriptionWrapper.classList.add("gapped");
+      jsMdbDescButton.innerHTML = "Свернуть описание<span></span>";
+    }
 
-        if (isExpanded) {
-            // Если элементы сворачиваются, выполняем прокрутку вниз до кнопки
-            jsMdbDescButton.scrollIntoView({behavior: 'smooth', block: 'end'});
-        }
-    };
+    if (isExpanded) {
+      // Если элементы сворачиваются, выполняем прокрутку вниз до кнопки
+      jsMdbDescButton.scrollIntoView({behavior: 'smooth', block: 'end'});
+    }
+
+  };
 }
 
 
@@ -102,10 +105,10 @@ const videoItems = document.querySelectorAll('.card_video_item');
 
 // Перебираем каждый элемент
 videoItems.forEach((item) => {
-    if (item.classList.contains('full-width')) {
-        const titleElement = item.querySelector('.card_video_title');
-        titleElement.style.display = 'none';
-    }
+  if (item.classList.contains('full-width')) {
+    const titleElement = item.querySelector('.card_video_title');
+    titleElement.style.display = 'none';
+  }
 });
 
 // еще один слайдер
@@ -115,15 +118,15 @@ const dots = document.querySelectorAll('.card-fullpage-dot');
 let currentImageIndex = 0;
 
 function setActiveImage(index) {
-    imageContainer.style.transform = `translateX(-${index * 100}%)`;
-    dots.forEach((dot, i) => {
-        dot.classList.toggle('active', i === index);
-    });
+  imageContainer.style.transform = `translateX(-${index * 100}%)`;
+  dots.forEach((dot, i) => {
+    dot.classList.toggle('active', i === index);
+  });
 }
 
 dots.forEach((dot, index) => {
-    dot.addEventListener('click', () => {
-        currentImageIndex = index;
-        setActiveImage(currentImageIndex);
-    });
+  dot.addEventListener('click', () => {
+    currentImageIndex = index;
+    setActiveImage(currentImageIndex);
+  });
 });
