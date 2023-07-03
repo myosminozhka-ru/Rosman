5a7d7b73634aa7df17f455d6e01ca9defdd76da5
mrmazyboy <mrmazyboy@gmail.com>
2023-07-03 16:34:17 +0300

    pretty

--- src/js/sliders/slider.js
+++ src/js/sliders/slider.js
@@ -1,65 +1,65 @@
-
-window.addEventListener("DOMContentLoaded", () => {
-    console.log("Loaded swiper");
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('Loaded swiper');
 });
 
 // init Swiper:
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: false,
-    // Navigation arrows
-    navigation: {
-        nextEl: '.button-next',
-        prevEl: '.button-prev',
+  // Optional parameters
+  loop: false,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+  autoHeight: true,
+  slidesPerView: 'auto',
+  spaceBetween: 8,
+  breakpoints: {
+    // настройки для разных разрешений
+    900: {
+      slidesPerView: 5,
+      spaceBetween: 30,
     },
-    autoHeight:true,
-    slidesPerView: 'auto',
-    spaceBetween: 8,
-    breakpoints: {// настройки для разных разрешений
-        900: {
-            slidesPerView: 5,
-            spaceBetween: 30,
-
-        },
-    }
+  },
 });
 const swiperImage = new Swiper('.swiper-image', {
-    // Optional parameters
-    loop: false,
-    // Navigation arrows
-    navigation: {
-        nextEl: '.button-next',
-        prevEl: '.button-prev',
+  // Optional parameters
+  loop: false,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+  autoHeight: true,
+  slidesPerView: 'auto',
+  spaceBetween: 8,
+  breakpoints: {
+    // настройки для разных разрешений
+    900: {
+      slidesPerView: 5,
+      spaceBetween: 30,
     },
-    autoHeight:true,
-    slidesPerView: 'auto',
-    spaceBetween: 8,
-    breakpoints: {// настройки для разных разрешений
-        900: {
-            slidesPerView: 5,
-            spaceBetween: 30,
-
-        },
-    }
+  },
 });
 // init Swiper:
 const swiperText = new Swiper('.swiper-text', {
-    // Optional parameters
-    loop: false,
-    // Navigation arrows
-    navigation: {
-        nextEl: '.button-next',
-        prevEl: '.button-prev',
+  // Optional parameters
+  loop: false,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+  autoHeight: true,
+  slidesPerView: 0.9,
+  spaceBetween: 10,
+  breakpoints: {
+    // настройки для разных разрешений
+    900: {
+      slidesPerView: 1,
+      spaceBetween: 60,
     },
-    autoHeight: true,
-    slidesPerView: 0.9,
-    spaceBetween: 10,
-    breakpoints: {// настройки для разных разрешений
-        900: {
-            slidesPerView: 1,
-            spaceBetween: 60,
-        },
-    }
+  },
 });
 // const swiperElephant = new Swiper('.elephant-swiper', {
 //     // Optional parameters
@@ -71,21 +71,21 @@ const swiperText = new Swiper('.swiper-text', {
 //     },
 // });
 const runStroke = new Swiper('.swiper-container', {
-    loop: false,
-    autoplay: {
-        delay: 20,
-        disableOnInteraction: false, // Включаем автоматическую прокрутку при взаимодействии пользователя
-    },
-    speed: 6000,
-    slidesPerView: 6,
+  loop: false,
+  autoplay: {
+    delay: 20,
+    disableOnInteraction: false, // Включаем автоматическую прокрутку при взаимодействии пользователя
+  },
+  speed: 6000,
+  slidesPerView: 6,
 });
 const runStrokeReverse = new Swiper('.swiper-container-reverse', {
-    loop: false,
-    autoplay: {
-        delay: 20,
-        disableOnInteraction: false, // Включаем автоматическую прокрутку при взаимодействии пользователя
-        reverseDirection: true, // Прокрутка в обратном направлении
-    },
-    speed: 6000,
-    slidesPerView: 6,
+  loop: false,
+  autoplay: {
+    delay: 20,
+    disableOnInteraction: false, // Включаем автоматическую прокрутку при взаимодействии пользователя
+    reverseDirection: true, // Прокрутка в обратном направлении
+  },
+  speed: 6000,
+  slidesPerView: 6,
 });
