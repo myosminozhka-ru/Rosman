d9de38f17b69ef9dc310d13ba924d643fb92d15e
clamator <kalmah23@gmail.com>
2023-07-18 12:33:02 +0600

    сделал страницу случайной книги

--- src/js/random-book/random-book.js
+++ src/js/random-book/random-book.js
@@ -1,27 +1,60 @@
 const randomBookButton = document.querySelector('.js-randomize-book');
 if (randomBookButton) {
   randomBookButton.addEventListener('click', function (event) {
+
+    // тут собираем активные чекбоксы
+    const selectedGenres = Array.from(document.querySelectorAll('#genres input:checked')).map(input => input.id);
+    const selectedAges = Array.from(document.querySelectorAll('#ages input:checked')).map(input => input.id);
+
+    // тут объект на отправку на бэк
+    const requestData = {
+      genres: selectedGenres,
+      ages: selectedAges,
+    };
+
+    // тут имитация отправки запроса на бэк
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    // тут отдельно все чекбоксы
+    const checkboxElements = document.querySelectorAll(
+      '.js-footer-heading-policy-agreement.cb-f-rb.checkbox_main input:checked'
+    );
+
+    // находим кнопки
     const activeDropDownsButtons = document.querySelectorAll(
       '.more-down-button.active-js'
     );
     const activeDropDownsContent = document.querySelectorAll(
       '.more-down-content.active-js'
     );
-
-    const checkboxElements = document.querySelectorAll(
-      '.js-footer-heading-policy-agreement.checkbox_main input:checked'
-    );
-
+    // тут классы кнопок,меняем стили
     activeDropDownsButtons.forEach(function (activeDDB) {
       activeDDB.classList.toggle('active-js');
     });
     activeDropDownsContent.forEach(function (activeDDC) {
       activeDDC.classList.toggle('active-js');
     });
+    // убираем галки
     checkboxElements.forEach(function (checkbox) {
       checkbox.checked = false;
     });
-
+    // меняем название кнопки
     randomBookButton.innerText = 'Подобрать ещё раз';
+    // пока оставлю так по стилям,
+    const literallyBook = document.querySelector(".random-book-card")
+    literallyBook.classList.remove('hidden')
+
   });
 }
