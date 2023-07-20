ec90a75e67e5413e639a5a093c262a8e5f5b7543
AlexSavusia <mr.savusya@gmail.com>
2023-07-20 13:32:34 +0700

    header fix

--- src/js/scripts/main.js
+++ src/js/scripts/main.js
@@ -79,22 +79,22 @@ window.addEventListener('DOMContentLoaded', () => {
   //     }
   //   });
   // }
-  const navbar = document.querySelector('.navigation');
+   const navbar = document.querySelector('.navigation');
+  const contentNavBarUl = document.querySelector('.nav-list');
   if (navbar) {
-    navbar.addEventListener('click', function (event) {
+    contentNavBarUl.addEventListener('click', function (event) {
       const contentNavBar =
         document.getElementsByClassName('navigation-content');
-      const contentNavBarUl = document.querySelector('.nav-list');
+
       const activeP = document.querySelector('p.active-js');
       const p = contentNavBarUl.getElementsByTagName('p');
       if (activeP != null) {
-        if (!activeP.contains(event.target)) {
+
           for (let i = 0; i < p.length; i++) {
             if (p[i].classList.contains('active-js')) {
               p[i].classList.remove('active-js');
             }
           }
-        }
       }
       for (let i = 0; i < contentNavBar.length; i++) {
         if (contentNavBar[i].id === event.target.classList.value) {
@@ -537,6 +537,14 @@ function toggleSearch() {
   } else {
     mobileMenu.classList.add('hide');
   }
+
+  const headerSearch = document.getElementById('search-header');
+    const searchResult = document.getElementById('search-result-header');
+  if (headerSearch && headerSearch.value) {
+    searchResult.classList.remove('hidden')
+  } else {
+    searchResult.classList.add('hidden')
+  }
 }
 
 function changeLang(lang) {
@@ -554,6 +562,11 @@ function changeLang(lang) {
 }
 
 function closeBar() {
+  const contentNavBar =
+      document.getElementsByClassName('navigation-content');
+  const contentNavBarUl = document.querySelector('.nav-list');
+  const activeP = document.querySelector('p.active-js');
+  const p = contentNavBarUl.getElementsByTagName('p');
   for (let i = 0; i < p.length; i++) {
     if (p[i].classList.contains('active-js')) {
       p[i].classList.remove('active-js');
@@ -739,3 +752,39 @@ if (allButtons) {
 
 // js-cl-pok Показать ещё
 // js-cl-pos Посмотреть все
+const allButtonsTwo = document.querySelectorAll('.js-cl-btn-2');
+
+if (allButtonsTwo) {
+  allButtonsTwo.forEach((button) => {
+    button.addEventListener('click', function () {
+      const siblingElement = button.previousElementSibling;
+      siblingElement.classList.toggle('max-h-[43rem]');
+
+      if (button.innerText === 'Свернуть') {
+        if (
+            button.classList.contains('js-cl-pok') &&
+            button.classList.contains('js-cl-pos')
+        ) {
+          button.innerText = 'Показать все результаты';
+        } else if (button.classList.contains('js-cl-pok')) {
+          button.innerText = 'Показать все результаты';
+        } else if (button.classList.contains('js-cl-pos')) {
+          button.innerText = 'Посмотреть все';
+        }
+      } else {
+        button.innerText = 'Свернуть';
+      }
+    });
+  });
+}
+
+const headerSearch = document.getElementById('search-header');
+headerSearch.addEventListener('input', function () {
+  const searchResult = document.getElementById('search-result-header');
+  if (headerSearch && headerSearch.value) {
+    searchResult.classList.remove('hidden')
+  } else {
+    searchResult.classList.add('hidden')
+  }
+
+});
