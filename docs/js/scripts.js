246bb56d02806159ab0341d61df71c4eafe1b6d7
clamator <kalmah23@gmail.com>
2023-07-27 20:02:46 +0600

    страница КАТЕГОРИЯ ЖАНРА-2

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1125,107 +1125,6 @@ if (buttonsPag) {
 }
 
 
-
-const swiperDetailedPage = new Swiper('.swiper-detailed-page', {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.button-next',
-    prevEl: '.button-prev',
-  },
-  autoHeight: true,
-  slidesPerView: 3,
-  spaceBetween: 16,
-  breakpoints: {
-    950: {
-      slidesPerView: 3,
-      spaceBetween: 16,
-    },
-    500: {
-      slidesPerView: 2,
-      spaceBetween: 8,
-    },
-    0: {
-      slidesPerView: 1.25,
-      spaceBetween: 8,
-    },
-  },
-});
-
-const swiperDetailedPageVideo = new Swiper('.swiper-detailed-page-video', {
-  // Optional parameters
-  loop: false,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.button-next',
-    prevEl: '.button-prev',
-  },
-  autoHeight: true,
-  slidesPerView: 2,
-  spaceBetween: 16,
-  breakpoints: {
-    950: {
-      slidesPerView: 2,
-    },
-    500: {
-      slidesPerView: 2,
-    },
-    0: {
-      slidesPerView: 1.2,
-    },
-  },
-});
-
-const swiperImidzhPageEmps = new Swiper('.swiper-imidzh-page', {
-  // Optional parameters
-  loop: false,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.button-next',
-    prevEl: '.button-prev',
-  },
-  autoHeight: true,
-  slidesPerView: 2,
-  spaceBetween: 16,
-  breakpoints: {
-    950: {
-      slidesPerView: 2,
-    },
-    500: {
-      slidesPerView: 2,
-    },
-    0: {
-      slidesPerView: 1.25,
-      spaceBetween: 24,
-    },
-  },
-});
-const swiperCatalogProjects = new Swiper('.swiper-catalog-projects', {
-  // Optional parameters
-  loop: false,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.button-next',
-    prevEl: '.button-prev',
-  },
-  autoHeight: true,
-  slidesPerView: 2,
-  spaceBetween: 16,
-  breakpoints: {
-    950: {
-      slidesPerView: 2,
-    },
-    550: {
-      slidesPerView: 2,
-    },
-    0: {
-      slidesPerView: 1.2,
-      spaceBetween: 16,
-    },
-  },
-});
-
 // карточкам будет дан специальный класс - js_traceable_item,
 // а элементам js-bg-changeable, которые будут менять фон,- js-hovered,
 // событие будет генерироваться на заход в целом на карточку - mouseenter/mouseleave
@@ -1378,6 +1277,107 @@ window.addEventListener('resize', function () {
 //     });
 //   }
 // });
+
+
+const swiperDetailedPage = new Swiper('.swiper-detailed-page', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+  autoHeight: true,
+  slidesPerView: 3,
+  spaceBetween: 16,
+  breakpoints: {
+    950: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+    },
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 8,
+    },
+    0: {
+      slidesPerView: 1.25,
+      spaceBetween: 8,
+    },
+  },
+});
+
+const swiperDetailedPageVideo = new Swiper('.swiper-detailed-page-video', {
+  // Optional parameters
+  loop: false,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+  autoHeight: true,
+  slidesPerView: 2,
+  spaceBetween: 16,
+  breakpoints: {
+    950: {
+      slidesPerView: 2,
+    },
+    500: {
+      slidesPerView: 2,
+    },
+    0: {
+      slidesPerView: 1.2,
+    },
+  },
+});
+
+const swiperImidzhPageEmps = new Swiper('.swiper-imidzh-page', {
+  // Optional parameters
+  loop: false,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+  autoHeight: true,
+  slidesPerView: 2,
+  spaceBetween: 16,
+  breakpoints: {
+    950: {
+      slidesPerView: 2,
+    },
+    500: {
+      slidesPerView: 2,
+    },
+    0: {
+      slidesPerView: 1.25,
+      spaceBetween: 24,
+    },
+  },
+});
+const swiperCatalogProjects = new Swiper('.swiper-catalog-projects', {
+  // Optional parameters
+  loop: false,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+  autoHeight: true,
+  slidesPerView: 2,
+  spaceBetween: 16,
+  breakpoints: {
+    950: {
+      slidesPerView: 2,
+    },
+    550: {
+      slidesPerView: 2,
+    },
+    0: {
+      slidesPerView: 1.2,
+      spaceBetween: 16,
+    },
+  },
+});
 
 document.addEventListener('DOMContentLoaded', function () {
   const brandDropdowns = document.querySelectorAll(
@@ -1927,6 +1927,295 @@ if (randomBookButton) {
   });
 }
 
+window.addEventListener('DOMContentLoaded', () => {
+  const rem = (16 / 100) * 65.2;
+
+  new Swiper('.js-toys-articles', {
+    loop: false,
+    navigation: {
+      nextEl: '.toys-articles-swiper-button-next',
+      prevEl: '.toys-articles-swiper-button-prev',
+    },
+
+    slidesPerView: 1.2,
+    autoHeight: true,
+    spaceBetween: rem * 1.6,
+    breakpoints: {
+      639: {
+        slidesPerView: 2,
+      },
+      868: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  new Swiper('.js-toys-brands', {
+    loop: false,
+    navigation: {
+      nextEl: '.toys-brands-swiper-button-next',
+      prevEl: '.toys-brands-swiper-button-prev',
+    },
+
+    slidesPerView: 1,
+    autoHeight: true,
+    spaceBetween: rem * 1.6,
+    breakpoints: {
+      639: {
+        slidesPerView: 2,
+      },
+      868: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
+
+  new Swiper('.js-toys-event-tape', {
+    loop: false,
+    navigation: {
+      nextEl: '.toys-event-tape-swiper-button-next',
+      prevEl: '.toys-event-tape-swiper-button-prev',
+    },
+
+    slidesPerView: 2,
+    autoHeight: true,
+    spaceBetween: rem * 1.6,
+    breakpoints: {
+      // 639: {
+      //   slidesPerView: 3,
+      // },
+      868: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
+
+  new Swiper('.js-toys-bestsellers', {
+    loop: false,
+    navigation: {
+      nextEl: '.bestsellers-swiper-button-next',
+      prevEl: '.bestsellers-swiper-button-prev',
+    },
+    pagination: {
+      el: '.bestsellers-swiper-pagination',
+      type: 'fraction',
+      renderFraction: function (currentClass, totalClass) {
+        return (
+          '<span class="text-slider-fraction body-m ' +
+          currentClass +
+          '"></span>' +
+          ' ' +
+          '	&nbsp;' +
+          '<span class="text-slider-fraction body-m"> из </span> ' +
+          '	&nbsp;' +
+          '<span class="text-slider-fraction body-m ' +
+          totalClass +
+          '"></span>'
+        );
+      },
+    },
+    autoHeight: true,
+    slidesPerView: 1,
+    spaceBetween: rem * 2.4 * 2,
+    breakpoints: {
+      900: {
+        spaceBetween: rem * 1.2 * 2,
+      },
+    },
+  });
+
+  new Swiper('.js-toys-banner', {
+    loop: false,
+    navigation: {
+      nextEl: '.toys-swiper-button-next',
+      prevEl: '.toys-swiper-button-prev',
+    },
+    pagination: {
+      el: '.toys-swiper-pagination',
+    },
+    autoHeight: true,
+    slidesPerView: 1,
+    spaceBetween: rem * 2.4 * 2,
+    breakpoints: {
+      900: {
+        spaceBetween: rem * 1.2 * 2,
+      },
+    },
+  });
+
+  const swiper = new Swiper('.swiper', {
+    loop: false,
+    navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
+    },
+    autoHeight: true,
+    slidesPerView: 'auto',
+    spaceBetween: 8,
+    breakpoints: {
+      900: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+      },
+    },
+  });
+  const swiperBook = new Swiper('.swiper-book', {
+    loop: false,
+    navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
+    },
+    autoHeight: true,
+    slidesPerView: 'auto',
+    spaceBetween: 8,
+    breakpoints: {
+      320: {
+        slidesPerView: 2.1,
+        spaceBetween: 30,
+      },
+      900: {
+        slidesPerView: 4.2,
+        spaceBetween: 60,
+      },
+    },
+  });
+
+  const swiperOneBook = new Swiper('.swiper-one-book', {
+    loop: false,
+    navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
+    },
+    autoHeight: true,
+    slidesPerView: 'auto',
+    spaceBetween: 8,
+    breakpoints: {
+      320: {
+        slidesPerView: 0.9,
+        spaceBetween: 20,
+      },
+      900: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+    },
+  });
+
+  const personsSLider = new Swiper('.persons-slider', {
+    navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
+    },
+    autoHeight: true,
+    slidesPerView: 1.2,
+    spaceBetween: 24,
+  });
+
+  const swiperImage = new Swiper('.swiper-image', {
+    loop: false,
+    navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
+    },
+    autoHeight: true,
+    slidesPerView: 'auto',
+    spaceBetween: 8,
+    breakpoints: {
+      900: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+      },
+    },
+  });
+  const swiperText = new Swiper('.swiper-text', {
+    loop: false,
+    navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
+    },
+    autoHeight: true,
+    slidesPerView: 0.9,
+    spaceBetween: 10,
+    breakpoints: {
+      900: {
+        slidesPerView: 1,
+        spaceBetween: 60,
+      },
+    },
+  });
+
+  const runStroke = new Swiper('.swiper-container', {
+    loop: false,
+    autoplay: {
+      delay: 20,
+      disableOnInteraction: false,
+    },
+    speed: 6000,
+    slidesPerView: 6,
+  });
+
+  const runStrokeReverse = new Swiper('.swiper-container-reverse', {
+    loop: false,
+    autoplay: {
+      delay: 20,
+      disableOnInteraction: false,
+      reverseDirection: true,
+    },
+    speed: 6000,
+    slidesPerView: 6,
+  });
+});
+
+// слайдер для истории слона, слайдер, но не свипер,
+function scrollContent(direction) {
+  let container = document.getElementById('elephant-container');
+  let content = document.getElementById('elephant-slider');
+
+  if (direction === 'left') {
+    container.scrollLeft -= 400; // Измените значение 100 на желаемое расстояние для перемещения влево
+  } else if (direction === 'right') {
+    container.scrollLeft += 400; // Измените значение 100 на желаемое расстояние для перемещения вправо
+  }
+}
+
+// открытие блоков, когда прокручено до середины экрана
+function addActiveClassToAutoOpenElements() {
+  const autoOpenElements = document.querySelectorAll('.auto-open');
+
+  if (autoOpenElements.length > 0) {
+    autoOpenElements.forEach((element) => {
+      element.classList.add('active-js');
+    });
+  }
+}
+
+function handleScroll() {
+  const elephantHistory = document.querySelector('.elephant-history');
+
+  if (elephantHistory) {
+    const { top, bottom } = elephantHistory.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+
+    if (top < windowHeight / 4 && bottom > windowHeight) {
+      addActiveClassToAutoOpenElements();
+
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }
+}
+
+window.addEventListener('scroll', handleScroll);
+
 // let largeImg = document.querySelector('.largeImg');
 // let thumbs = document.querySelectorAll('.thumbs img');
 //
@@ -2402,295 +2691,6 @@ window.onresize = function () {
   updateSliderColHeight();
   // chooseSwiperItem();
 };
-
-window.addEventListener('DOMContentLoaded', () => {
-  const rem = (16 / 100) * 65.2;
-
-  new Swiper('.js-toys-articles', {
-    loop: false,
-    navigation: {
-      nextEl: '.toys-articles-swiper-button-next',
-      prevEl: '.toys-articles-swiper-button-prev',
-    },
-
-    slidesPerView: 1.2,
-    autoHeight: true,
-    spaceBetween: rem * 1.6,
-    breakpoints: {
-      639: {
-        slidesPerView: 2,
-      },
-      868: {
-        slidesPerView: 3,
-      },
-    },
-  });
-
-  new Swiper('.js-toys-brands', {
-    loop: false,
-    navigation: {
-      nextEl: '.toys-brands-swiper-button-next',
-      prevEl: '.toys-brands-swiper-button-prev',
-    },
-
-    slidesPerView: 1,
-    autoHeight: true,
-    spaceBetween: rem * 1.6,
-    breakpoints: {
-      639: {
-        slidesPerView: 2,
-      },
-      868: {
-        slidesPerView: 3,
-      },
-      1024: {
-        slidesPerView: 4,
-      },
-    },
-  });
-
-  new Swiper('.js-toys-event-tape', {
-    loop: false,
-    navigation: {
-      nextEl: '.toys-event-tape-swiper-button-next',
-      prevEl: '.toys-event-tape-swiper-button-prev',
-    },
-
-    slidesPerView: 2,
-    autoHeight: true,
-    spaceBetween: rem * 1.6,
-    breakpoints: {
-      // 639: {
-      //   slidesPerView: 3,
-      // },
-      868: {
-        slidesPerView: 3,
-      },
-      1024: {
-        slidesPerView: 4,
-      },
-    },
-  });
-
-  new Swiper('.js-toys-bestsellers', {
-    loop: false,
-    navigation: {
-      nextEl: '.bestsellers-swiper-button-next',
-      prevEl: '.bestsellers-swiper-button-prev',
-    },
-    pagination: {
-      el: '.bestsellers-swiper-pagination',
-      type: 'fraction',
-      renderFraction: function (currentClass, totalClass) {
-        return (
-          '<span class="text-slider-fraction body-m ' +
-          currentClass +
-          '"></span>' +
-          ' ' +
-          '	&nbsp;' +
-          '<span class="text-slider-fraction body-m"> из </span> ' +
-          '	&nbsp;' +
-          '<span class="text-slider-fraction body-m ' +
-          totalClass +
-          '"></span>'
-        );
-      },
-    },
-    autoHeight: true,
-    slidesPerView: 1,
-    spaceBetween: rem * 2.4 * 2,
-    breakpoints: {
-      900: {
-        spaceBetween: rem * 1.2 * 2,
-      },
-    },
-  });
-
-  new Swiper('.js-toys-banner', {
-    loop: false,
-    navigation: {
-      nextEl: '.toys-swiper-button-next',
-      prevEl: '.toys-swiper-button-prev',
-    },
-    pagination: {
-      el: '.toys-swiper-pagination',
-    },
-    autoHeight: true,
-    slidesPerView: 1,
-    spaceBetween: rem * 2.4 * 2,
-    breakpoints: {
-      900: {
-        spaceBetween: rem * 1.2 * 2,
-      },
-    },
-  });
-
-  const swiper = new Swiper('.swiper', {
-    loop: false,
-    navigation: {
-      nextEl: '.button-next',
-      prevEl: '.button-prev',
-    },
-    autoHeight: true,
-    slidesPerView: 'auto',
-    spaceBetween: 8,
-    breakpoints: {
-      900: {
-        slidesPerView: 5,
-        spaceBetween: 30,
-      },
-    },
-  });
-  const swiperBook = new Swiper('.swiper-book', {
-    loop: false,
-    navigation: {
-      nextEl: '.button-next',
-      prevEl: '.button-prev',
-    },
-    autoHeight: true,
-    slidesPerView: 'auto',
-    spaceBetween: 8,
-    breakpoints: {
-      320: {
-        slidesPerView: 2.1,
-        spaceBetween: 30,
-      },
-      900: {
-        slidesPerView: 4.2,
-        spaceBetween: 60,
-      },
-    },
-  });
-
-  const swiperOneBook = new Swiper('.swiper-one-book', {
-    loop: false,
-    navigation: {
-      nextEl: '.button-next',
-      prevEl: '.button-prev',
-    },
-    autoHeight: true,
-    slidesPerView: 'auto',
-    spaceBetween: 8,
-    breakpoints: {
-      320: {
-        slidesPerView: 0.9,
-        spaceBetween: 20,
-      },
-      900: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      1200: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-    },
-  });
-
-  const personsSLider = new Swiper('.persons-slider', {
-    navigation: {
-      nextEl: '.button-next',
-      prevEl: '.button-prev',
-    },
-    autoHeight: true,
-    slidesPerView: 1.2,
-    spaceBetween: 24,
-  });
-
-  const swiperImage = new Swiper('.swiper-image', {
-    loop: false,
-    navigation: {
-      nextEl: '.button-next',
-      prevEl: '.button-prev',
-    },
-    autoHeight: true,
-    slidesPerView: 'auto',
-    spaceBetween: 8,
-    breakpoints: {
-      900: {
-        slidesPerView: 5,
-        spaceBetween: 30,
-      },
-    },
-  });
-  const swiperText = new Swiper('.swiper-text', {
-    loop: false,
-    navigation: {
-      nextEl: '.button-next',
-      prevEl: '.button-prev',
-    },
-    autoHeight: true,
-    slidesPerView: 0.9,
-    spaceBetween: 10,
-    breakpoints: {
-      900: {
-        slidesPerView: 1,
-        spaceBetween: 60,
-      },
-    },
-  });
-
-  const runStroke = new Swiper('.swiper-container', {
-    loop: false,
-    autoplay: {
-      delay: 20,
-      disableOnInteraction: false,
-    },
-    speed: 6000,
-    slidesPerView: 6,
-  });
-
-  const runStrokeReverse = new Swiper('.swiper-container-reverse', {
-    loop: false,
-    autoplay: {
-      delay: 20,
-      disableOnInteraction: false,
-      reverseDirection: true,
-    },
-    speed: 6000,
-    slidesPerView: 6,
-  });
-});
-
-// слайдер для истории слона, слайдер, но не свипер,
-function scrollContent(direction) {
-  let container = document.getElementById('elephant-container');
-  let content = document.getElementById('elephant-slider');
-
-  if (direction === 'left') {
-    container.scrollLeft -= 400; // Измените значение 100 на желаемое расстояние для перемещения влево
-  } else if (direction === 'right') {
-    container.scrollLeft += 400; // Измените значение 100 на желаемое расстояние для перемещения вправо
-  }
-}
-
-// открытие блоков, когда прокручено до середины экрана
-function addActiveClassToAutoOpenElements() {
-  const autoOpenElements = document.querySelectorAll('.auto-open');
-
-  if (autoOpenElements.length > 0) {
-    autoOpenElements.forEach((element) => {
-      element.classList.add('active-js');
-    });
-  }
-}
-
-function handleScroll() {
-  const elephantHistory = document.querySelector('.elephant-history');
-
-  if (elephantHistory) {
-    const { top, bottom } = elephantHistory.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
-
-    if (top < windowHeight / 4 && bottom > windowHeight) {
-      addActiveClassToAutoOpenElements();
-
-      window.removeEventListener('scroll', handleScroll);
-    }
-  }
-}
-
-window.addEventListener('scroll', handleScroll);
 
 // скрипт для слежения за вводом в поля инпутов
 const inputFields = document.querySelectorAll('.js-traceable-item-vacancy');
